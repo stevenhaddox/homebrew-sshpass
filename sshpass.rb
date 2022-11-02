d935689cb10d029985f4cf90309af1890ed0e27a
Steven Haddox <steven.haddox@gmail.com>
2022-11-02 09:49:42 -0600

    Update to 1.08

--- sshpass.rb
+++ sshpass.rb
@@ -1,7 +1,7 @@
 class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
-  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.06/sshpass-1.06.tar.gz"
-  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.08/sshpass-1.08.tar.gz"
+  sha256 "8bdacb8ca4ecf82c84a5effa2f75a8bf4ed9dd6f9d030f308fe11cffb3830b02"
 
   def install
     system "./configure", "--disable-debug",
